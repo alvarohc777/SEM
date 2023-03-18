f0d967b27886a362643e5739b4c9707604c479bd
alvarohc777 <aherrada@uninorte.edu.co>
2023-03-17 22:34:44 -0500

    Create notebook to work with load change

---
+++ utils/components.py
@@ -0,0 +1,10 @@
+three_phase_switch = [
+    "C switch load tri\n",
+    "  TRIINATRILDA       -1.      1.E3                                             0\n",
+    "  TRIINBTRILDB       -1.      1.E3                                             0\n",
+    "  TRIINCTRILDC       -1.      1.E3                                             0\n",
+]
+single_phase_switch = [
+    "C switch load mono\n",
+    "  MONOINMONOLD       -1.      1.E3                                             0\n",
+]
