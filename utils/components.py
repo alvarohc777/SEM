a2f810a51f70f69100120b3e0dcba35148c57b39
alvarohc777 <aherrada@uninorte.edu.co>
2023-03-19 16:37:22 -0500

    Updated switch components to be able to open at any time

--- utils/components.py
+++ utils/components.py
@@ -1,10 +1,10 @@
 three_phase_switch = [
     "C switch load tri\n",
-    "  TRIINATRILDA       -1.      1.E3                                             0\n",
-    "  TRIINBTRILDB       -1.      1.E3                                             0\n",
-    "  TRIINCTRILDC       -1.      1.E3                                             0\n",
+    "  TRIINATRILDA       -1.      1.E3      999.                                   0\n",
+    "  TRIINBTRILDB       -1.      1.E3      999.                                   0\n",
+    "  TRIINCTRILDC       -1.      1.E3      999.                                   0\n",
 ]
 single_phase_switch = [
     "C switch load mono\n",
-    "  MONOINMONOLD       -1.      1.E3                                             0\n",
+    "  MONOINMONOLD       -1.      1.E3      999.                                   0\n",
 ]
