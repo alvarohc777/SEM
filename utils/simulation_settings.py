98a951fefe64b37f5e2ff2e356e3238f6f97311a
orozcohc <orozcohc@gmail.com>
2024-12-19 08:25:30 -0500

    corregir funcionalidad de tiempo de la simulación

--- utils/simulation_settings.py
+++ utils/simulation_settings.py
@@ -2,7 +2,7 @@
 def update_simulation_time(lines, simulation_time, simulation_time_idx):
     simulation_time = str(simulation_time).rjust(8)
     simulation_time_line = lines[simulation_time_idx]
-    lines[
-        simulation_time_idx
-    ] = f"{simulation_time_line[:8]}{simulation_time}{simulation_time[16:]}\n"
+    lines[simulation_time_idx] = (
+        f"{simulation_time_line[:8]}{simulation_time}{simulation_time_line[16:]}"
+    )
     return lines
